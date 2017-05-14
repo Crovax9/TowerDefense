d8f0fada2f341505ede559c0b1e352031ab00be6
Crovax9 <Crovax9@gmail.com>
2017-05-14 21:55:53 +0900

    Tower LockOnMehtod
    Tower Model Add & LockOnMethod Add

---
+++ Assets/Turret.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+    private Transform target;
+    public float range = 15f;
+
+    public string enemyTag = "Enemy";
+
+    public Transform rotatePoint;
+    public float damping = 10f;
+
+    private void Start()
+    {
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
+    }
+
+    private void Update()
+    {
+        if (target == null)
+        {
+            return;
+        }
+
+        TurretLookRotation();
+    }
+
+    void TurretLookRotation()
+    {
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(rotatePoint.rotation, lookRotation, Time.deltaTime * damping).eulerAngles;
+        rotatePoint.rotation = Quaternion.Euler(0f, rotation.y, 0f);
+    }
+
+    void UpdateTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistance = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+
+        foreach(GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistance)
+            {
+                shortestDistance = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
+
+        if (nearestEnemy != null && shortestDistance <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+
+    //DrawRangeMethod
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, range);
+        
+    }
+}
